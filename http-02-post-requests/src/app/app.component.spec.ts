77929953f9467c0862f198f8ae2222497ebad61b
Sara <sara.667@outlook.com>
2023-07-18 20:28:20 -0600

    http practice finished

--- http-02-post-requests/src/app/app.component.spec.ts
+++ http-02-post-requests/src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-complete-guide-update'`, () => {
+  it(`should have as title 'angular-practice'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-complete-guide-update');
+    expect(app.title).toEqual('angular-practice');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-complete-guide-update!');
+    expect(compiled.querySelector('h1').textContent).toContain(
+      'Welcome to angular-practice!'
+    );
   });
 });
